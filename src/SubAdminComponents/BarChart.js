ccf7596a7b85fe789741a6859f23a84a2d54f8c2
Nafishsy <nafizahmed273273@gmail.com>
2022-08-23 13:17:14 +0600

    Trying Chart js

--- src/SubAdminComponents/BarChart.js
+++ src/SubAdminComponents/BarChart.js
@@ -2,27 +2,51 @@ import { Chart as ChartJS } from 'chart.js/auto'
 import React from "react";
 import { Bar,Pie} from 'react-chartjs-2'
 
-const data ={
-    labels: ["Red","Blue","ASDASDue","zxC","zxcz",],
+const data = {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
     datasets: [
         {
-            data: [13,123,123,1231,232],
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
             backgroundColor: [
-                "red",
-                "blue",
-                "green",
-                "grey",
-                "orange",
-            ]
-        }
-    ]
-}
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+            ],
+            borderWidth: 1,
+        },
+    ],
+};
+
+const options = {
+    scales: {
+        yAxes: [
+            {
+                ticks: {
+                    beginAtZero: true,
+                },
+            },
+        ],
+    },
+};
+
 const BarChart = () =>{
     return(
         <div align='center' style={{width: '1000px', height: '10px'}}>
             <center>
             
-            <Pie data={data}/> 
+            <Bar data={data} options={options}/> 
             </center>
 
         </div>
